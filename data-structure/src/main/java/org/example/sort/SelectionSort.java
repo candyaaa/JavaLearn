4e522fb4eeef38b567ae7f9270e0ee26eb409df8
shiKui <shikui@gxdataway.com>
2021-03-28 17:38:40 +0800

    Feat #time 30h #comment selection sort

---
+++ data-structure/src/main/java/org/example/sort/SelectionSort.java
@@ -0,0 +1,20 @@
+package org.example.sort;
+
+/**
+ * 选择排序
+ */
+public class SelectionSort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
