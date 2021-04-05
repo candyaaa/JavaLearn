358665647400343f83d30c81022b0997d32bd388
shikui <670569467@qq.com>
2021-04-05 21:45:47 +0800

    Feat #time 10m #beanFactoryPostProcessor.postProcessBeanFactory

---
+++ spring-source/src/main/java/org/example/spring/beanfactorypostprocessor/BeanFactoryPostProcessorConfig.java
@@ -0,0 +1,8 @@
+package org.example.spring.beanfactorypostprocessor;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("org.example.spring.beanfactorypostprocessor")
+public class BeanFactoryPostProcessorConfig {
+    
+}
