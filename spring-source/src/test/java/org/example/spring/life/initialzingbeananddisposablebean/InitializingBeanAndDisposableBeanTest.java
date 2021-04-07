75b1fd8dfa5e1d0a49db05bd5c6a7df780e879ec
shikui <670569467@qq.com>
2021-04-07 11:54:09 +0800

    Feat #time 10m #InitializingBean and DisposableBean demo

---
+++ spring-source/src/test/java/org/example/spring/life/initialzingbeananddisposablebean/InitializingBeanAndDisposableBeanTest.java
@@ -0,0 +1,12 @@
+package org.example.spring.life.initialzingbeananddisposablebean;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class InitializingBeanAndDisposableBeanTest {
+    @Test
+    public void initializingBeanAndDisposableBeanTest() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(InitializingBeanAndDisposableBeanConfig.class);
+        applicationContext.close();
+    }
+}
