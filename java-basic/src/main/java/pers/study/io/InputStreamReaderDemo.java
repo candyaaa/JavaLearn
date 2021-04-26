6b89390699f1b93d0644b7349c34d3695fe8bcfe
shikui <670569467@qq.com>
2021-04-26 21:36:33 +0800

    Feat #time 5m #io input and output

---
+++ java-basic/src/main/java/pers/study/io/InputStreamReaderDemo.java
@@ -0,0 +1,16 @@
+package pers.study.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputStreamReaderDemo {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(new FileInputStream(Constants.PATH),"utf-8");
+        int len;
+        while ((len = reader.read()) != -1) {
+            System.out.print((char) len);
+        }
+        reader.close();
+    }
+}
