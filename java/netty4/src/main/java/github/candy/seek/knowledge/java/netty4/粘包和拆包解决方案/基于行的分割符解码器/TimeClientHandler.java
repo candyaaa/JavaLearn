5d982dc96279f4344124ad7a9a804d8a01a16f8e
candy <670569467@qq.com>
2021-10-19 11:34:22 +0800

    feat:基于行的分隔符解码器

---
+++ java/netty4/src/main/java/github/candy/seek/knowledge/java/netty4/粘包和拆包解决方案/基于行的分割符解码器/TimeClientHandler.java
@@ -0,0 +1,43 @@
+package github.candy.seek.knowledge.java.netty4.粘包和拆包解决方案.基于行的分割符解码器;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import java.nio.charset.StandardCharsets;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author candy
+ * @date 2021/10/18
+ */
+@Slf4j
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+    private byte[] req;
+
+    public TimeClientHandler() {
+        req = ("QUERY TIME REQUEST" + System.getProperty("line.separator")).getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 100; i++) {
+            ByteBuf buffer = Unpooled.buffer(req.length);
+            buffer.writeBytes(req);
+            ctx.writeAndFlush(buffer);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String time = (String) msg;
+        log.info("Now is:{}", time);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.info("Unexpected exception from downstream:{}", cause.getMessage());
+        ctx.close();
+    }
+}
