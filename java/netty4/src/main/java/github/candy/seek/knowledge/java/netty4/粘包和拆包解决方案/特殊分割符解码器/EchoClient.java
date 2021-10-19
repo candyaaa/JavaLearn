f19eb871147fd94e3a1e5d576f8da9c382f7680c
candy <670569467@qq.com>
2021-10-19 11:29:45 +0800

    candy
    feat:特殊分割符解码器

---
+++ java/netty4/src/main/java/github/candy/seek/knowledge/java/netty4/粘包和拆包解决方案/特殊分割符解码器/EchoClient.java
@@ -0,0 +1,52 @@
+package github.candy.seek.knowledge.java.netty4.粘包和拆包解决方案.特殊分割符解码器;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import java.nio.charset.StandardCharsets;
+import org.junit.Test;
+
+/**
+ * @author candy
+ * @date 2021/10/18
+ */
+public class EchoClient {
+    public void connect(String host, int port) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        try {
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ByteBuf byteBuf = Unpooled.copiedBuffer("$_".getBytes(StandardCharsets.UTF_8));
+                            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, byteBuf));
+                            ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect(host, port).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    @Test
+    public void start() {
+        connect("127.0.0.1", 9999);
+    }
+}
